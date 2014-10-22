e1cb85f08b1e1189f23f525be18b1a55da3f8556
anaelChardan <anael.chardan@gmail.com>
2014-10-22 15:51:32 +0200

    test

--- src/Taquin/Taquin.java
+++ src/Taquin/Taquin.java
@@ -2,6 +2,6 @@ package Taquin;
 
 public class Taquin {
     public static void main(String[] args) {
-        System.out.println("Hello Piou Piou");
+        System.out.println("Hello Piou bite");
     }
 }
