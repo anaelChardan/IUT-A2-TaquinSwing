83225ba8f2b9cf0d5eb0d115bf9053e74f57c1f9
anaelChardan <anael.chardan@gmail.com>
2014-10-23 21:31:27 +0200

    Added some necessary class to project, and there default members

---
+++ src/Taquin/Click.java
@@ -0,0 +1,11 @@
+package Taquin;
+
+import javax.swing.event.MouseInputAdapter;
+
+/**
+ * Created by Ananas on 23/10/14.
+ */
+public class Click extends MouseInputAdapter {
+
+
+}
---
+++ src/Taquin/Grid.java
@@ -0,0 +1,14 @@
+package Taquin;
+
+import java.awt.*;
+
+/**
+ * Created by Ananas on 23/10/14.
+ */
+public class Grid extends Canvas {
+    private int _nbRows;
+    private int _nbColumns;
+    private int _axis;
+    private int _ordinate;
+
+}
