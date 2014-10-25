a23e97c3ff2dac4cc4f3469b31ebcf71acbd815a
anaelChardan <anael.chardan@gmail.com>
2014-10-25 23:00:56 +0200

    Step 1 and 4 of the exercise done and work well, very well

--- src/NoRules/Grid.java
+++ src/NoRules/Grid.java
@@ -1,9 +1,86 @@
 package NoRules;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Ananas on 25/10/14.
  */
 public class Grid extends JPanel {
+    private int _nbRows;
+    private int _nbColumns;
+    private int _axis;
+    private int _ordinate;
+    private int _columnWidth = 50;
+    private int _rowHeight = 50 ;
+    private int _initialRow = 5;
+    private int _initialColumn = 5;
+
+    public Grid() {
+        this._nbRows = this._initialRow;
+        this._nbColumns = this._initialColumn;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        //////////////Calcul of the beginning of the axis and the ordinate////////////////
+        _axis = ( this.getSize().width / 2 ) - ( this._nbColumns * this._columnWidth / 2 );
+        _ordinate = ( this.getSize().height / 2 ) - ( this._nbRows * this._rowHeight / 2 );
+
+        /////////////Draw horizontal Line////////////
+        for ( int i = 0 ; i <= _nbRows ; i ++ )
+            g.drawLine( _axis, _ordinate + _rowHeight * i , _axis + ( _nbColumns * _columnWidth ),  _ordinate + _rowHeight * i );
+
+        /////////////Draw Vertical Line//////////////
+        for ( int i = 0; i <= _nbColumns; i++ )
+            g.drawLine( _axis + _columnWidth * i, _ordinate, _axis + _columnWidth * i, _ordinate + ( _nbRows * _rowHeight ) );
+
+    }
+
+    public int addColumn() {
+        this._nbColumns ++;
+        repaint();
+        if ( ( this._nbColumns + 1 ) * this._columnWidth < this.getSize().width ) {
+            return 0;
+        } else
+            return -1;
+
+
+    }
+
+    public int addRow() {
+        this._nbRows++;
+        repaint();
+        if ( ( this._nbRows + 1 ) * this._rowHeight < this.getSize().height ) {
+            return 0;
+        } else
+            return -1;
+
+    }
+
+    public int throwColumn() {
+        this._nbColumns--;
+        repaint();
+        if ( this._nbColumns == this._initialColumn ) {
+            return -1;
+        } else
+            return 0;
+    }
+
+    public int throwRow() {
+        this._nbRows--;
+        repaint();
+        if ( this._nbRows == this._initialRow ) {
+            return -1;
+        } else
+            return 0;
+
+    }
+
+    public void reset() {
+        this._nbRows = this._initialRow;
+        this._nbColumns = this._initialColumn;
+        repaint();
+    }
 }
--- src/NoRules/Window.java
+++ src/NoRules/Window.java
@@ -1,9 +1,114 @@
 package NoRules;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Ananas on 25/10/14.
  */
 public class Window extends JFrame {
+
+    private Grid _theGrid = new Grid();
+    private JButton _addColumn = new JButton("Add a Column");
+    private JButton _addRow = new JButton("Add a row");
+    private JButton _throwColumn = new JButton("Throw a Column");
+    private JButton _throwRow = new JButton("Throw a Row");
+    private JButton _reset = new JButton("Reset");
+
+    private JPanel container = new JPanel();
+
+    public Window() {
+        this.setTitle("Taquin");
+        this.setSize(700, 700);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+
+        container.setLayout(new BorderLayout());
+        container.add(_theGrid, BorderLayout.CENTER);
+
+
+        ///////Button to add a Column///////
+        _addColumn.addActionListener( new addColumn() );
+
+        ///////Button to add a Row///////
+        _addRow.addActionListener( new addRow() );
+
+        ///////Button to throw a Column///////
+        _throwColumn.addActionListener( new throwColumn() );
+        _throwColumn.setEnabled( false );
+
+        //////Button to throw a Row//////////
+        _throwRow.addActionListener( new throwRow() );
+        _throwRow.setEnabled( false );
+
+        //////Button to reset//////////
+        _reset.addActionListener( new reset() );
+
+
+        JPanel north = new JPanel();
+        north.add(_addColumn);
+        north.add(_addRow);
+        north.add(_throwColumn);
+        north.add(_throwRow);
+        north.add(_reset);
+
+        //////////Gestion de notre container////
+        container.add(north,BorderLayout.NORTH);
+        this.setContentPane(container);
+        this.setVisible(true);
+    }
+
+
+    private class addColumn implements ActionListener {
+        public void actionPerformed(ActionEvent arg0) {
+            if ( _theGrid.addColumn() == -1 ) {
+                _addColumn.setEnabled(false);
+            }
+            _throwColumn.setEnabled(true);
+        }
+    }
+
+
+    private class addRow implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if ( _theGrid.addRow() == -1 ) {
+                _addRow.setEnabled(false);
+            }
+            _throwRow.setEnabled(true);
+        }
+    }
+
+    private class throwColumn implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+           if ( _theGrid.throwColumn() == -1 ) {
+               _throwColumn.setEnabled(false);
+           }
+           _addColumn.setEnabled(true);
+
+        }
+    }
+
+    private class throwRow implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (  _theGrid.throwRow() == -1 ) {
+                _throwRow.setEnabled(false);
+            }
+            _addRow.setEnabled(true);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Window _game = new Window();
+    }
+
+    private class reset implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            _theGrid.reset();
+        }
+    }
 }
+
