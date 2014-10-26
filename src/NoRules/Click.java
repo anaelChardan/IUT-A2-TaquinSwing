4b5d7c8934c6ee317c03ba882888d7e82862724e
anaelChardan <anael.chardan@gmail.com>
2014-10-26 23:17:28 +0100

    To understand how MouseInputAdapter worked

--- src/NoRules/Click.java
+++ src/NoRules/Click.java
@@ -1,9 +1,14 @@
 package NoRules;
 
 import javax.swing.event.MouseInputAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by Ananas on 25/10/14.
  */
 public class Click extends MouseInputAdapter {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        super.mouseClicked(e);
+    }
 }
